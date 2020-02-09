f28b73a0e8d62b0717995a01f46164fa3dc50fd1
Zheming Chen <60445515+qqwwsyz@users.noreply.github.com>
2020-02-09 11:50:02 -0800

    Add files via upload

---
+++ lab2/task2/arith.c
@@ -0,0 +1,18 @@
+/*  Example: C program to find area of a circle */
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a, b, c;
+
+  printf("Enter radius (in mm):\n");
+  scanf("%f", &r);
+  b = r *0.0393700;
+  a = PI * b * b  ;
+
+  printf("Circle's area is %3.2f (sq in).\n", a);
+  c = 2 * PI * b;
+  printf("Its circumference is %3.2f (in).\n", c);
+}

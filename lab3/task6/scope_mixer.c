0e4369ff46cd345119a079a2aeb6d34a291b4d25
Guo Tang <gt56733n@pace.edu>
2020-02-18 07:47:24 -0800

    Task6 OK

--- lab3/task6/scope_mixer.c
+++ lab3/task6/scope_mixer.c
@@ -56,11 +56,13 @@ int main(void)
 
 	int n = 7;
 	printf("main : m=%d, n=%d\n", m, n);
+	//main : m=1, n=7
 	U(n);
 	printf("main : m=%d, n=%d\n", m, n);
+	//main : m=7, n=7
 	A();
 	printf("main : m=%d, n=%d\n\n\n", m, n);
-
+	//main : m=3, n=7
 	// -------------------------------------------------
 	// Below is a block, which may declare its own local
 	// variables:
@@ -73,8 +75,10 @@ int main(void)
 		int n = 100;
 
 		printf("block : m=%d, n=%d\n", m, n);
+		//block : m=3, n=100
 		U(n);
 		printf("block : m=%d, n=%d\n", m, n);
+		//block : m=100, n=100
 		// -------------------------------------------------
 		// A local variable m is now declared.
 		// References to m from here to the end of the
@@ -83,14 +87,17 @@ int main(void)
 		int m = -40;
 
 		printf("block : m=%d, n=%d\n", m, n);
+		//block : m=-40, n=100
 		val(m);
 		printf("block : m=%d\n", m);
+		//block : m=-40
 		addr(&m);
 		printf("block : m=%d\n", m);
-
+		//block : m=5
 	}
 
 	printf("main : m=%d, n=%d\n\n\n", m, n);
+	//main : m=100, n=7
 	//
 	// ---------------------------------------------------------------------
 	return 0;
@@ -107,6 +114,8 @@ void U(int n)
 {
 	m = n;
 	printf("   U: m=%d, n=%d\n", m, n);
+	//   U: m=7, n=7
+	//   U: m=100, n=100
 }
 
 // ----------------------------------------------------------------------------
@@ -118,14 +127,17 @@ void A(void)
 		extern int m;
 		m = 5; 
 		printf("  global m=%d\n",  m);
+		//   global m=5
 	}
 	printf("   A: m=%d\n", m);
+	//   A: m=3
 }
 // ----------------------------------------------------------------------------
 
 void val(int x)
 {
 	printf("val  : x=%d\n", x);
+	//val  : x=-40
 	x = 5;
 }
 // ----------------------------------------------------------------------------
@@ -133,5 +145,6 @@ void val(int x)
 void addr(int *x)
 {
 	printf("ref  : *x=%d\n", *x);
+	//ref  : *x=-40
 	*x = 5;
 }

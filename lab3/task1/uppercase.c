c773fb82e2bd7cb7e0049c2ff7501c6e616082ff
qqwwsyz <jasonchenzheming@gmail.com>
2020-02-19 10:21:08 -0800

    task1

--- lab3/task1/uppercase.c
+++ lab3/task1/uppercase.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
+#include<string.h>
+
 
 int main(int argc, char *argv[]) {
-/* example:
- *  char str[10090];
-   int ch, n = 0;
 
-   while ((ch = getchar()) != EOF && n < 1000) {
+ char str[25];
+   int ch, n=0;
+
+   while ((ch = getchar()) != EOF ) {
       str[n] = ch;
       ++n;
    }
-
-   for (int i = 0; i < n; ++i)
-      putchar(str[i]);
-
+  for (int i = 0; i<=strlen(str);i++) 
+{
+    if ((str[i] >= 97) && (str[i] <= 122)) 
+        str[i] = str[i]-32;      
+        
+  
+}                                
+printf("\nUpper Case String is: %s",str);
 
    return 0;
-
- */
-
-    return 0;
 }
 

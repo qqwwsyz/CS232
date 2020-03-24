614179ef0545aa078aadc87ece1c68ee4e2a4525
qqwwsyz <jasonchenzheming@gmail.com>
2020-03-24 13:13:53 -0700

    task1

---
+++ lab6/task1/snode.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "snode.h"
+#include <string.h>
+#include <stdlib.h>
+
+struct snode *snode_create(char *s) 
+{
+	int len = strlen(s) + 1;
+	struct snode * my_snode = malloc(sizeof(struct snode));
+	my_snode->str = (char*)malloc(len * sizeof(char));
+	strcpy(my_snode->str, s);
+	my_snode->next = NULL;
+	return my_snode;  
+}
+void snode_destroy(struct snode * s) 
+{
+free(s->str);
+free(s);
+}
+ 

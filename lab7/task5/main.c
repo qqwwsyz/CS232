7701113999bb8812b65da045043a121ef3fdc0e4
Guo Tang <gt56733n@pace.edu>
2020-04-08 13:33:32 -0700

    reverse

--- lab7/task5/main.c
+++ lab7/task5/main.c
@@ -1,13 +1,20 @@
 char password[8] = "secret";
-int main() {
+int main(){
     char input[8];
+    int temp;
     printf("please enter your password\n");
     scanf("%s", input);
-
+    temp = 0;
+    while( temp < 8 ){
+        if(input[temp]>= 65 && input[temp] < 91){
+            input[temp] = input[temp] + 32;
+        }
+        temp++;
+    }
     if(strcmp(input, password)==0) {
         return 0;
-    } else {
+    } 
+    else{
         return -1;
     }
-
 }
